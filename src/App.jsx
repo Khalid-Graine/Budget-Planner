c2a44fd5dab29d3448e3054e442485b2293d24e6
Khalid Graine <khalidgraine1@gmail.com>
2023-11-16 11:02:31 +0100

    make the data dynamic in the first section

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,44 @@
-import TheTitle from './components/TheTitle'
-import  Dashboard  from './components/Dashboard'
-import ExpensesList from './components/ExpensesList'
-import AddExpense from './components/AddExpense'
+import TheTitle from "./components/TheTitle";
+import Dashboard from "./components/Dashboard";
+import ExpensesList from "./components/ExpensesList";
+import AddExpense from "./components/AddExpense";
+import { useState } from "react";
 function App() {
- 
+  const [Budget,SetBudget] = useState({
+    Budget: "2000",
+    Remaining: "1510",
+    Spent: "490",
+  });
+  const [Expenses,setExpense] = useState({
+    Name: "car",
+    Cost: 10,
+  });
+
+
 
   return (
     <>
-     <div className='bg-gray-200 h-screen w-full p-2'>
+      <div className="bg-gray-200 h-screen w-full p-2">
+        <div>
+          <TheTitle text="My Budget Planner" />
+          <Dashboard  info={Budget} />
+        </div>
 
+        <div>
+          <TheTitle text="Expenses" />
+          <form action="">
+            <input type="text" />
+          </form>
+          <ExpensesList />
+        </div>
 
-      <div>
-        <TheTitle text="My Budget Planner" />
-        <Dashboard />
+        <div>
+          <TheTitle text="Add Expense" />
+          <AddExpense />
+        </div>
       </div>
-
-
-      <div>
-      <TheTitle text="Expenses" />
-       <form action="">
-        <input type="text" />
-       </form>
-      <ExpensesList />
-      </div>
-
-
-      <div>
-      <TheTitle text="Add Expense" />
-       <AddExpense />
-      </div>
-
-
-
-     </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,16 +1,22 @@
- const Dashboard = () => {
+const Dashboard = ({ info }) => {
   return (
-    <div className='flex flex-col gap-2'>
-          <div className='bg-blue-300 py-2 px-1'>
-           <p>Budget: <span className='font-bold'>$2000</span></p>
-          </div>
-          <div className='bg-blue-300 py-2 px-1'>
-           <p>Remaining: <span className='font-bold'>$1510</span></p>
-          </div>
-          <div className='bg-blue-300 py-2 px-1'>
-           <p>Spent so far: <span className='font-bold'>$490</span></p>
-          </div>
-        </div>
-  )
-}
+    <div className="flex flex-col gap-2">
+      <div className="bg-blue-300 py-2 px-1">
+        <p>
+          Budget: <span className="font-bold">${info.Budget}</span>
+        </p>
+      </div>
+      <div className="bg-blue-300 py-2 px-1">
+        <p>
+          Remaining: <span className="font-bold">${info.Remaining}</span>
+        </p>
+      </div>
+      <div className="bg-blue-300 py-2 px-1">
+        <p>
+          Spent so far: <span className="font-bold">${info.Spent}</span>
+        </p>
+      </div>
+    </div>
+  );
+};
 export default Dashboard;
